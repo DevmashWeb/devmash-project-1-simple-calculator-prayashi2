12ffa67a81bc19e56c123d24dbb2a1a4b0739ebf
PRAYASHI <prayashikumari2@gmail.com>
2016-10-01 11:12:50 +0530

    second commit

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ function c(val)
 {
 document.getElementById("d").value=val;
 }
+
 function v(val)
 {
 document.getElementById("d").value+=val;
 }
+
+function back() 
+{
+   //delete last letter function.
+    var value = document.getElementById("d").value;
+    document.getElementById("d").value = value-1;
+
+   document.getElementById("d").value = value.substr(0, value.length - 1);
+
+}
+
 function e() 
 { 
 try 
